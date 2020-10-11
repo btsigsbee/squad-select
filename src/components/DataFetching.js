e047299a367d9c80ae5019af1d2641ee6456cca5
Bryce <bstsigsbee@gmail.com>
2020-10-11 17:52:38 -0400

    connected to api. displayed teams and logo.

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,34 +1,64 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, setState} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/row';
+import Col from 'react-bootstrap/col';
 
 
-function DataFetching(props){
-    const [gameData, setGameData] = useState(null) 
-    useEffect(()=> {
-        async function fetchData(){const response = await fetch("https://v2.api-football.com/fixtures/team/165/next/1", {
+ 
+
+export default function DataFetching(){
+    const [gameData, setGameData] = useState([]);
+    useEffect( ()=> {
+           
+        fetch("https://v2.api-football.com/fixtures/team/165/next/1", {
           "method": "GET",
           "headers": {
               "x-rapidapi-host": "v3.football.api-sports.io",
               "x-rapidapi-key": "101667642ea71b7b5066153b76cf897a"
           }
-      });
-      const data = await response.json();
-      setGameData(data.api.fixtures[0]);
-    }
-      fetchData();
+
+      })
+      .then((response)=> response.json())
+      .then((data)=> {setGameData(data.api.fixtures)})
         
-    
         
-    }, []);
+        
+    }, [])
+    console.log(gameData);
     return(
     <div>
-    {gameData.map((game)=> {
-        const hometeam=game.homeTeam;
-
-        return(<h3>{hometeam}</h3>)
-    })}
+         
+        <Row className='mt-2'>
+        <Col>
+        <h3>Home</h3>
+        
+        </Col>
+        <Col>
+            <h3>Away</h3>
+        </Col>
+        </Row>
+        <Row>
+        {gameData.length >0?(
+            gameData.map((game) =>(<Col className="col-lg-5" ><Row className="offset-md-4"key={game.homeTeam.team_id} >
+            
+            <h1 className="text-nowrap">{game.homeTeam.team_name}</h1>
+            <img className="img-fluid rounded float-right offset-md-1" width='50px' src={game.homeTeam.logo}></img>
+            </Row>
+            </Col>)))
+            
+            :(<h1>Loading</h1>)}
+            <Col><h1>V</h1></Col>
+            
+            {gameData.length >0?(
+            gameData.map((game) =>(<Col className="col-lg-5"><Row key={game.awayTeam.team_id}>
+            <img className="img-fluid rounded float-left" width='50px' src={game.awayTeam.logo}></img>
+            <h1 className="text-nowrap offset-md-1">{game.awayTeam.team_name}</h1>
+            
+            </Row></Col>)))
+            
+            :(<h1>Loading</h1>)}
+        </Row>
     </div>
 )
 }
-
-
-export default DataFetching
